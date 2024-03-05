0b453c34f5a552909f73eac616dd22dcccf452be
André Oliveira <andredev426@gmail.com>
2024-03-05 19:46:24 -0300

    feat: Calculator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Calculator from '../components/Calculator'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+
+    <h1 style={{marginBottom: 50, fontFamily: 'sans-serif'}}>Projeto Calculadora</h1>
+      <Calculator />
+   </div> 
+   
+  )
+}
+
+export default App
